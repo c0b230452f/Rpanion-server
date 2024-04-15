72c73b903c33de238f22647e74413976b8796b3c
TsuyoshiKawamura <kawamura@tajisoft.jp>
2024-04-15 23:41:03 +0900

    WIP fix home page

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,35 +1,28 @@
 import React from 'react'
 import { Route, Routes, Link } from 'react-router-dom'
 
-import About from './about.js'
 import Home from './home.js'
-import NetworkConfig from './networkconfig.js'
-import VideoPage from './video.js'
 import FCConfig from './flightcontroller.js'
 import LogBrowser from './logBrowser.js'
 import NetworkClients from './networkClients.js'
 import NTRIPPage from './ntripcontroller.js'
-import AdhocConfig from './adhocwifi.js'
-import CloudConfig from './cloud.js'
 import VPN from './vpnconfig'
+
+import Logo from './images/logo.webp'
 
 function AppRouter () {
   return (
     <div id="wrapper" className="d-flex">
       <div id="sidebar-wrapper" className="bg-light border-right">
-      <div id="sidebarheading" className="sidebar-heading">Rpanion Web UI</div>
+      <div id="sidebarheading" className="sidebar-heading">
+        <img src={Logo} width="100px" />
+      </div>
         <div id="sidebar-items" className="list-group list-group-flush">
           <Link className='list-group-item list-group-item-action bg-light' to="/">Home</Link>
           <Link className='list-group-item list-group-item-action bg-light' to="/flightlogs">Flight Logs</Link>
           <Link className='list-group-item list-group-item-action bg-light' to="/controller">Flight Controller</Link>
           <Link className='list-group-item list-group-item-action bg-light' to="/ntrip">NTRIP Config</Link>
-          <Link className='list-group-item list-group-item-action bg-light' to="/network">Network Config</Link>
-          <Link className='list-group-item list-group-item-action bg-light' to="/adhoc">Adhoc Wifi Config</Link>
-          <Link className='list-group-item list-group-item-action bg-light' to="/apclients">Access Point Clients</Link>
-          <Link className='list-group-item list-group-item-action bg-light' to="/video">Video Streaming</Link>
-          <Link className='list-group-item list-group-item-action bg-light' to="/cloud">Cloud Upload</Link>
           <Link className='list-group-item list-group-item-action bg-light' to="/vpn">VPN Config</Link>
-          <Link className='list-group-item list-group-item-action bg-light' to="/about">About</Link>
         </div>
       </div>
 
@@ -38,14 +31,9 @@ function AppRouter () {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/controller" element={<FCConfig />} />
-            <Route exact path="/network" element={<NetworkConfig />} />
-            <Route exact path="/about" element={<About />} />
-            <Route exact path="/video" element={<VideoPage />} />
             <Route exact path="/flightlogs" element={<LogBrowser />} />
             <Route exact path="/apclients" element={<NetworkClients />} />
             <Route exact path="/ntrip" element={<NTRIPPage />} />
-            <Route exact path="/adhoc" element={<AdhocConfig />} />
-            <Route exact path="/cloud" element={<CloudConfig />} />
             <Route exact path="/vpn" element={<VPN/>} />
             <Route element={NoMatch} />
           </Routes>
--- src/home.js
+++ src/home.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import basePage from './basePage.js'
 
+import roverImage from './images/spark-kit-rover.webp'
+
 class Home extends basePage {
   constructor (props) {
     super(props)
@@ -17,16 +19,18 @@ class Home extends basePage {
   }
 
   renderTitle () {
-    return 'Home Page'
+    return 'Spark Kit'
   }
 
   renderContent () {
     return (
       <div>
-        <p>Welcome to the Rpanion-server home page</p>
-        <p>Use the links on the left to configure the system</p>
-        <p><a href='https://github.com/stephendade/Rpanion-server'>Rpanion-server website</a></p>
-        <p><a href='https://www.docs.rpanion.com/software/rpanion-server'>Rpanion-server documentation</a></p>
+        <p>
+        <img src={roverImage} width="50%" height="50%" /><br/>
+          ArduPilot標準なローバー。</p>
+        <p><a href='https://www.robot-to-society.com/lp/sparkkit'>Spark Kitについて</a></p>
+        <p><a href='https://github.com/rtos-org/spark-kit.git'>ドキュメント</a></p>
+        <p><a href='https://github.com/rtos-org/spark-kit.git'>コミュニティ</a></p>
       </div>
     )
   }
